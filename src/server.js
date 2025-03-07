81a6c8b4430e45fc7aff78592cc32a69def59667
alpha33tt <benwilsonj087@gmail.com>
2025-03-07 02:31:26 -0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const dns = require('dns'); // Use the built-in dns module
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
-app.use(express.json());
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Validate email domain with MX record lookup
 function isValidDomain(domain) {
@@ -42,6 +44,11 @@ app.post('/validate-emails', async (req, res) => {
     }
 
     res.json({ validEmails });
+});
+
+// If no other routes match, serve index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
